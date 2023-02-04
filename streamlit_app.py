86ca2f9b295d8e49f81fce2fe037358f6d8ed205
BANUZEENATH <124422226+BANUZEENATH@users.noreply.github.com>
2023-02-04 01:06:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner Breakfast Menu')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text(Omega & Blueberry Oatmeal)
+
+streamlit.text(Kale,Spinach & Rocket Smoothie )
+
+streamlit.text('Hard-Boiled Free-Range Egg')
